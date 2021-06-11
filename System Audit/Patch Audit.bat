2311a240505db75dffc2781ed4de160a019c82e5
Mazars <85697696+mazarsITAuditTeam@users.noreply.github.com>
2021-06-11 12:53:58 +0530

    Update Patch Audit.bat

--- System Audit/Patch Audit.bat
+++ System Audit/Patch Audit.bat
@@ -1,41 +1,45 @@
 cmd.exe /c set
-arp -a > autit_op.txt
-ipconfig /all >> autit_op.txt
-ipconfig /displaydns >> autit_op.txt
-route print >> autit_op.txt
-net view >> autit_op.txt
-netstat -nao >> autit_op.txt
-netstat -vb >> autit_op.txt
-netstat -ns >> autit_op.txt
-net accounts >> autit_op.txt
-net session >> autit_op.txt
-net group >> autit_op.txt
-net share >> autit_op.txt
-net localgroup >> autit_op.txt
-net user >> autit_op.txt
-net view /domain >> autit_op.txt
-netsh firewall show config >> autit_op.txt
-net group administrators >> autit_op.txt
-tasklist /svc >> autit_op.txt
-net localgroup administrators >> autit_op.txt
-netsh wlan show drivers >> autit_op.txt
-netsh wlan show networks mode=bssid >> autit_op.txt
-gpresult /SCOPE COMPUTER /Z >> autit_op.txt
-cmdkey /list >> autit_op.txt
-gpresult /SCOPE USER /Z >> autit_op.txt
-netsh wlan show interfaces >> autit_op.txt
-netsh wlan show profiles >> autit_op.txt
-wmic group list >> autit_op.txt
-wmic useraccount list >> autit_op.txt
-wmic service get name,captions >> autit_op.txt
-wmic volume list brief >> autit_op.txt
-wmic logicaldisk get description,filesystem,name,size >> autit_op.txt
-wmic netlogin get name,lastlogon,badpasswordcount >> autit_op.txt
-wmic netclient list brief >> autit_op.txt
-wmic netuse get name,username,connectiontype,localname >> autit_op.txt
-wmic share get name,path >> autit_op.txt
-wmic nteventlog get path,filename,writeable >> autit_op.txt
-wmic startup list full >> autit_op.txt
-wmic product get name,version >> autit_op.txt
-wmic rdtoggle list >> autit_op.txt
-wmic qfe >> autit_op.txt
+arp -a > Patch_audit.txt
+ipconfig /all >> Patch_audit.txt
+ipconfig /displaydns >> Patch_audit.txt
+route print >> Patch_audit.txt
+net view >> Patch_audit.txt
+netstat -nao >> Patch_audit.txt
+netstat -vb >> Patch_audit.txt
+netstat -ns >> Patch_audit.txt
+net accounts >> Patch_audit.txt
+net session >> Patch_audit.txt
+net session /domain>> Patch_audit.txt
+net group >> Patch_audit.txt
+net group /domain>> Patch_audit.txt
+net share >> Patch_audit.txt
+net share /domain>> Patch_audit.txt
+net localgroup >> Patch_audit.txt
+net localgroup /domain>> Patch_audit.txt
+net user >> Patch_audit.txt
+net view /domain >> Patch_audit.txt
+netsh firewall show config >> Patch_audit.txt
+net group administrators >> Patch_audit.txt
+tasklist /svc >> Patch_audit.txt
+net localgroup administrators >> Patch_audit.txt
+netsh wlan show drivers >> Patch_audit.txt
+netsh wlan show networks mode=bssid >> Patch_audit.txt
+gpresult /SCOPE COMPUTER /Z >> Patch_audit.txt
+cmdkey /list >> Patch_audit.txt
+gpresult /SCOPE USER /Z >> Patch_audit.txt
+netsh wlan show interfaces >> Patch_audit.txt
+netsh wlan show profiles >> Patch_audit.txt
+wmic group list >> Patch_audit.txt
+wmic useraccount list >> Patch_audit.txt
+wmic service get name,captions >> Patch_audit.txt
+wmic volume list brief >> Patch_audit.txt
+wmic logicaldisk get description,filesystem,name,size >> Patch_audit.txt
+wmic netlogin get name,lastlogon,badpasswordcount >> Patch_audit.txt
+wmic netclient list brief >> Patch_audit.txt
+wmic netuse get name,username,connectiontype,localname >> Patch_audit.txt
+wmic share get name,path >> Patch_audit.txt
+wmic nteventlog get path,filename,writeable >> Patch_audit.txt
+wmic startup list full >> Patch_audit.txt
+wmic product get name,version >> Patch_audit.txt
+wmic rdtoggle list >> Patch_audit.txt
+wmic qfe >> Patch_audit.txt
